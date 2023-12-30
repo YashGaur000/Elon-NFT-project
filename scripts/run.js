ef9058bd8fc7d3116fa2ba4028a8f1f4773b1ca8
Kiyotaka-000 <103846663+YashGaur000@users.noreply.github.com>
2023-12-30 23:01:46 +0530

    first contract deployed

---
+++ scripts/run.js
@@ -0,0 +1,55 @@
+// // We require the Hardhat Runtime Environment explicitly here. This is optional
+// // but useful for running the script in a standalone fashion through `node <script>`.
+// //
+// // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// // will compile your contracts, add the Hardhat Runtime Environment's members to the
+// // global scope, and execute the script.
+// const hre = require("hardhat");
+
+// async function main() {
+//   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+//   const unlockTime = currentTimestampInSeconds + 2*60;
+
+//   const lockedAmount = hre.ethers.parseEther("2");
+
+//   const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
+//     value: lockedAmount,
+//   });
+
+//   await lock.waitForDeployment();
+
+//   console.log(
+//     `Lock with ${ethers.formatEther(
+//       lockedAmount
+//     )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+//   );
+// }
+
+// // We recommend this pattern to be able to use async/await everywhere
+// // and properly handle errors.
+// main().catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
+
+
+
+
+
+//                ELON'S CODE HERE
+
+
+const hre=require("hardhat")
+
+async function main(){
+console.log("Deploying Contract...");
+const elon=await hre.ethers.deployContract("ElonNFT")
+console.log("Contract Deployed to address",elon.target);
+}
+
+main()
+  .then(() =>process.exit(0))
+  .catch((error) => {
+  console.log("Error: " + error)
+  process.exit(1);
+});
